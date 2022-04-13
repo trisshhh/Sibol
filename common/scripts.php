35dda34548de3ea57932b55baf7cdf74c279afd5
MaureenDadap <maureendadap@gmail.com>
2022-04-14 06:26:36 +0800

    added correct bootstrap files

---
+++ common/scripts.php
@@ -0,0 +1,10 @@
+<!-- Vendor Files -->
+<script src="vendor/bootstrap/js/bootstrap.bundle.js"></script>
+
+<!-- user scripts -->
+<script src="js/scrollTop.js"></script>
+<script>
+    AOS.init({
+        once: true
+    });
+</script>
--- jobs-training-browse.php
+++ jobs-training-browse.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'common/components.php';
-include("common/connection.php");
+require_once 'utils/connection.php';
 ?>
 
 <!DOCTYPE html>
